32bd92b675c625c78434b3e0a26a72b330821978
nitatori <47695196+nitatori@users.noreply.github.com>
2019-02-20 22:34:20 -0300

    Add files via upload
    codicos

---
+++ aula.py
@@ -0,0 +1,12 @@
+
+a = 10
+b = 20
+print("valor de a : " , a)
+print ("valor de b : " , b)
+
+c = a
+a = b
+b = c
+print("valor de a : " , a)
+print("valor de b : " , b)
+print("valor de c : " , a)
---
+++ aula3.py
@@ -0,0 +1,18 @@
+# toda entrada do input é STR (string)
+idade = input("digite sua idade")
+# posso convereter de STR parea INT
+print("sua idade é" , idade)
+
+idade = in(idade)
+
+print("o tipo de variedade é" , type(idade) )
+
+idade = float(idade)
+
+# print ("sua idade é" , idade)
+print("o tipo da variavel é" , type (idade) )
+
+
+# calculando a metade
+metade = (idade / 2)+ 1
+print("A metade é" , metade )
