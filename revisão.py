a207dfcd092378a8d9c6d51ec0ed91410d1bf94a
nitatori <47695196+nitatori@users.noreply.github.com>
2019-03-27 22:30:18 -0300

    atividades de revisão de prova

---
+++ revisão.py
@@ -0,0 +1,28 @@
+Na programação, uma variável é um objeto uma posição, frequentemente localizada na memória capaz de reter e representar um valor ou expressão
+Para criar uma variável em Python basta escrever um nome que não tenha sido ainda definido, utilizar o comando de atribuição ( = ) e atribuir um valor
+
+trazem implementado o que é chamado de tipos primitivos, isto é, o tipo de dado mais genérico possível. tipos de dados mais comuns String , Inteiro
+Operadores aritméticos tradicionais de adição, subtração, multiplicação e divisão
+x=64
+x=40
+x=8
+A) Fornece um conjunto de funções para operações matemáticas, tais como funções trigonométricas, hiperbólicas, logaritmos, potência e arredondamentos.Todas as funções da biblioteca math.h retornam um valor do tipo double.
+
+B) import math
+x=math.sqrt(87)
+print(x)
+
+C)math.floor(1.1) = 1
+math.floor(3.3) = 3
+math.floor(-1.1) = -2
+
+D)math.ceil(1.1) = 2
+math.ceil(3.3) = 4
+math.ceil(-1.1) = -1
+E)
+
+F)
+
+11=Os operadores relacionais são: igual, diferente, maior, menor, maior ou igual, menor ou igual.
+os pricipais operadores relacionais sãodiferente dependendo da linguagem de programação, mas conhecendo os símbolos mais comuns, a maioria da linguagens de programação fica mais fácil aprender.
+As expressão relacional são: =, <>, >, <, >=, <=.
